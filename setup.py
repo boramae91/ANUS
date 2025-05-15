12a8e37ae8b6cd599a87eb7523b876d93e09fe65
boramae91 <boramae91@gmail.com>
2025-05-15 09:42:22 +0900

    [FIX] 참조겅로 절대경로로 수정
    - ex> README.md -> ANUS/README.md

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r", encoding="utf-8") as fh:
+with open("ANUS/README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("requirements.txt", "r", encoding="utf-8") as fh:
+with open("ANUS/requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read().splitlines()
 
 setup(
