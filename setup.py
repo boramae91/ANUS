f1c7fbf246c5d2924666e4c4a4d5aa0ab90cd858
boramae91 <boramae91@gmail.com>
2025-05-15 10:01:03 +0900

    run_setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 with open("C:/MyNewProject/ANUS/README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-with open("ANUS/requirements.txt", "r", encoding="utf-8") as fh:
+with open("C:/MyNewProject/ANUS/requirements.txt", "r", encoding="utf-8") as fh:
     requirements = fh.read().splitlines()
 
 setup(
